f559f3a0b23064629279c6eff3ff2f41de329d50
Akshay Jagtap <akshayjagtap900@gmail.com>
2022-02-26 12:20:05 +0530

    Added RegularExpressionProject

---
+++ RegularExpressionDemo/RegularExpressionDemo/SourceFiles/Main.cpp
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//																										//
+//	Problem Statement :                                                                                 //
+//	Read string from standard input following of duplicated word.										//
+//	The program should find places in the input where one word is followed immediately by itself.		//	
+//	Keep track of the largest number of times a single repetition occurs and which word is prepeated	//
+//	Print the maximum number of duplicates, or else print a messaage saying that no word was repeated.	//																						//
+//																										//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <regex>
+#include <string>
+
+int main()
+{
+
+
+
+
+	return 0;
+}
